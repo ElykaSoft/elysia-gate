ac5ac302bcba90bb2fd0ae22b95a93ae551987e0
ElysiaKafka <38758499+lucifermbbgh@users.noreply.github.com>
2024-02-08 23:43:12 +0800

    更新 elysia_dict.sql

--- ddl/20240209/elysia_dict.sql
+++ ddl/20240209/elysia_dict.sql
@@ -2,12 +2,12 @@ DROP TABLE IF EXISTS `elysia_dict`;
 CREATE TABLE `elysia_dict`
 (
     `ID`        INT AUTO_INCREMENT NOT NULL COMMENT 'ID',
-    `dict_code` VARCHAR(50)        NOT NULL COMMENT '字典码',
-    `dict_name` VARCHAR(100)       NOT NULL COMMENT '字典描述',
-    `dict_type` VARCHAR(50)        NOT NULL COMMENT '字典分类',
-    `status`    TINYINT(1)         NOT NULL DEFAULT 1 COMMENT '状态（0：禁用，1：启用）',
+    `DICT_CODE` VARCHAR(50)        NOT NULL COMMENT '字典码',
+    `DICT_NAME` VARCHAR(100)       NOT NULL COMMENT '字典描述',
+    `DICT_TYPE` VARCHAR(50)        NOT NULL COMMENT '字典分类',
+    `STATUS`    TINYINT(1)         NOT NULL DEFAULT 1 COMMENT '状态',
     PRIMARY KEY (`ID`) USING BTREE,
-    UNIQUE KEY `idx_dict_code_unique` (`dict_code`)
+    UNIQUE KEY `idx_dict_code_unique` (`DICT_CODE`)
 ) COMMENT ='字典表'
     ENGINE = InnoDB
     DEFAULT CHARSET = utf8mb4
